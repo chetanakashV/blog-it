6326dfe0c08489feef6cfef85e8b9e2c19b81462
shelockholmes999 <=>
2022-08-23 08:54:28 +0530

    second commit

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import {db} from '../firebase-config'
+import { useNavigate } from 'react-router';
 import { useParams } from 'react-router';
 import {collection, doc, getDocs,getDoc,deleteDoc} from 'firebase/firestore'
+import * as IoMdIcons from 'react-icons/io'
 
 import * as ImIcons from 'react-icons/im'
 
@@ -10,6 +12,7 @@ function Post(props) {
     const {id} = useParams();
     const userCollectionRef = collection(db,"posts");
     const mailid = localStorage.getItem("email");
+    const navigate = useNavigate();
     const deletepost = async (id) => {
         const userDoc = doc(db,"posts",id);
         await deleteDoc(userDoc,id).then(() => alert("post deleted!"));
@@ -23,14 +26,19 @@ function Post(props) {
         getPost();
     },[])
 
+    const clickHandler = () => {
+        navigate('/');
+    }
+
     return (
         <div style = {{width: "90%", position: "relative", left: "5%"}}>
+            <button onClick = {clickHandler} style = {{background:"fog", border: "none", position: "relative", right: "40%", size: "50px"}}> <IoMdIcons.IoMdArrowRoundBack/> Go back</button>
             {id}
             {post.map((post) => {return <div key = {post.title}> 
             <div style = {{border: "1px solid black", position : "relative", borderRadius:"15px"}}>
              <u><><i>{post.title}</i></> </u><br/> <br/>
-            <p  style = {{textAlign:"left"}}>{post.content}</p> <br/><span  style ={{maxWidth:"15px"}}> 
-           { localStorage.getItem("isAuth") &&  mailid == post.email && <button style ={{position: "relative", left:"55%", background: "white", border: "none",color: "red"}} onClick = {() => deletepost(post.id)} ><ImIcons.ImBin2/> </button>}
+            <p  style = {{textAlign:"left" , margin: "10px"}}>{post.content}</p> <br/><span  style ={{maxWidth:"15px"}}> 
+           { localStorage.getItem("isAuth") &&  mailid == post.email && <button style ={{position: "relative", left:"52%", background: "white", border: "none",color: "red"}} onClick = {() => deletepost(post.id)} ><ImIcons.ImBin2/> </button>}
                @{post.author}</span><br/>
             </div>
              <br/><br/></div>})}
