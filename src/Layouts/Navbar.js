85a9b63027b4966c85527ebcb218acdbb7d52f8f
chetanakashv <sherlockdetective69@gmail.com>
2024-08-28 15:53:24 +0530

    ..

--- src/Layouts/Navbar.js
+++ src/Layouts/Navbar.js
@@ -1,8 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink,BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import {auth} from '../firebase-config'
+import { getRemoteConfig, getValue,
+    fetchAndActivate
+ } from "firebase/remote-config";
 import './Navbar.css'
 
+
+// const remoteConfig = getRemoteConfig();
+// remoteConfig.settings.minimumFetchIntervalMillis = 1000;
+
+// async function fetchRemoteConfig() {
+//     try {
+//         const isFetched = await fetchAndActivate(remoteConfig);
+//         console.log("trial",isFetched);
+
+//         // if (isFetched) {
+//             console.log("Remote config fetched and activated:", isFetched);
+//             const bgColorValue = getValue(remoteConfig, "value"); // Assuming bg_color is a boolean
+//             console.log(bgColorValue)
+//             return bgColorValue;
+//         // } else {
+//         //     console.log("Remote config was not fetched.");
+//         //     return false; // Default to false if fetching failed
+//         // }
+//     } catch (error) {
+//         console.error("Error fetching remote config:", error);
+//         return false; // Default to false if there's an error
+//     }
+// }
 
 function Navbar(props) {
     //const navigate = useNavigate();
@@ -10,10 +36,14 @@ function Navbar(props) {
     const [clickLogin,setClickLogin] = useState(false);
     const [clickWP,setClickWP] = useState(false);
 
-    const handleClickHome  = () => {setClickHome(true); setClickLogin(false); setClickWP(false);}
+    const handleClickHome  = () => {setClickHome(true); setClickLogin(false); setClickWP(false); }
     const handleClickLogin  = () => {setClickHome(false); setClickLogin(true); setClickWP(false);}
     const handleClickWP  = () => {setClickHome(false); setClickLogin(false); setClickWP(true); }
     const logStatus = localStorage.getItem("isAuth");
+
+    const [color1, setColor] = useState("magenta"); 
+
+
     
 
     return (
